012cbef402216466acaf124046289300023eaa37
Daniel Fitzpatrick <dancyfits@gmail.com>
2018-01-22 09:09:23 -0800

    Switch from CRA to Parcel
    Die, webpack; I hate you.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./src/App";
 import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
